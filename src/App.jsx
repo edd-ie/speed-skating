68d9e9b1adf2f51403d9f2c48e6a295e8108ba7b
edd-ie <ogendoeddie@gmail.com>
2023-10-28 18:42:55 -0300

    Initial webpage setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Home from './components/Home'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
