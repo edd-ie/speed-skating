d449e27d3c6c84796976d7834ae9c53134fbf0fc
edd-ie <ogendoeddie@gmail.com>
2023-12-01 19:58:42 -0400

    bug fix

--- src/components/Directory/Directions.jsx
+++ src/components/Directory/Directions.jsx
@@ -18,7 +18,7 @@ export default function Directions() {
 
                 <iframe 
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d625.1739266419455!2d-66.64238073039509!3d45.924532198192836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ca42280843c9ea1%3A0x493d837090c5dad1!2sFredericton%20Amateur%20Speed%20Skating%20Club!5e1!3m2!1sen!2sca!4v1701473310526!5m2!1sen!2sca" 
-                    style={{border:"0", height:"94vh", width: "100vw"}}
+                    style={{border:"0", height:"90vh", width: "100vw"}}
                     allowfullscreen=""
                     loading="eager" 
                     referrerpolicy="no-referrer-when-downgrade">
